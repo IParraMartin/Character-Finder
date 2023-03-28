67993784ceb29cba9157be877b0ff862ae82aab6
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2023-03-28 16:18:02 -0500

    modified class

--- reldetc.py
+++ reldetc.py
@@ -1,46 +1,56 @@
 with open("DIRECTORY_PATH", "r") as document:
     text = document.read()
 
-def process_tokens(tokens, keyword_list):
 
-    keyword_found = False
-    keywords_seen = []
-    relationship_counts = {}
-    relationship_list = []
+keywords = ['SET CHARACTERS OR ENTITIES TO FIND']
 
-    for i, token in enumerate(tokens):
-    
-        if token in keyword_list:
 
-            keyword_found = True
-            keywords_seen.append(token)
-            
-            for j in range(i+1, min(i+21, len(tokens))):
+class CharacterLinker:
 
-                next_token = tokens[j]
+    def __init__(self):
+
+        pass
+
+    def process_tokens(self, tokens, keyword_list):
+
+        keyword_found = False
+        keywords_seen = [] 
+        relationship_counts = {}
+        relationship_list = []
+
+        for i, token in enumerate(tokens):
+        
+            if token in keyword_list:
+
+                keyword_found = True
+                keywords_seen.append(token)
                 
-                if next_token in keyword_list:
+                for j in range(i+1, min(i+21, len(tokens))):
 
-                    relationship_list.append((token, next_token))
-                    key = (token, next_token)
+                    next_token = tokens[j]
+                    
+                    if next_token in keyword_list:
 
-                    if key in relationship_counts:
+                        relationship_list.append((token, next_token))
+                        key = (token, next_token)
 
-                        relationship_counts[key] += 1
+                        if key in relationship_counts:
 
-                    else:
+                            relationship_counts[key] += 1
 
-                        relationship_counts[key] = 1
+                        else:
 
-                    print(f"Detected relationship: {token} - {next_token}")
-            
-    return keyword_found, keywords_seen, relationship_counts, relationship_list
+                            relationship_counts[key] = 1
+
+                        print(f"Detected relationship: {token} - {next_token}")
+                
+        return keyword_found, keywords_seen, relationship_counts, relationship_list
 
 
-keywords = ['SET_CHARACTERS']
 tokens = text.split()
 
-keyword_found, keywords_seen, relationship_counts, relationship_list = process_tokens(tokens, keywords)
+finder = CharacterLinker()
+keyword_found, keywords_seen, relationship_counts, relationship_list = finder.process_tokens(tokens, keywords)
 
 for relationship, count in relationship_counts.items():
 
